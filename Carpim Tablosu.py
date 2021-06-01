875dafe29f887c727d0e66a71fe16d84ff2c1e9c
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2021-06-01 23:06:53 +0300

    Çarpım Tablosu oluşturan kodlar

---
+++ Carpim Tablosu.py
@@ -0,0 +1,12 @@
+x = [1,2,3,4,5,6,7,8,9,10]
+y = [1,2,3,4,5,6,7,8,9,10]
+
+for i in x: # Çarpım Tablosu
+    print("")
+    print(i, "x ... Tablosu ")
+    print("*" * 20)
+
+    
+    for j in y:
+        print(i, "x", j, "=", i*j)
+
---
+++ Carpim Tablosu_2.py
@@ -0,0 +1,11 @@
+x = range(1,11)
+y = range(1,11)
+
+for i in x:
+    print(" ")
+    print(i, "x ... Tablosu")
+    print("_" * 20)
+    for j in y:
+        print(i, "x", j, "=", i * j)
+
+		
