acb688c6bfc9b2d8f7c17c676d44e7960482a833
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2023-12-29 19:19:44 +0300

    Pandas ve regex kodları eklendi

---
+++ Pandas ve regex/pandas ve regex.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import re
+
+df = pd.read_excel("pandas_re_veri.ods")
+# print(df)
+
+# MALZEME sütunundaki veriler içerisinde 2 ile 5 rakam bulunan değerleri gösterir.
+print(df.loc[df["MALZEME"].str.contains("\d{2,5}", regex=True)])
+
+
+def sayi(x):
+    desen = r"\d+"
+    deger = re.findall(desen, x)
+    return deger
+
+# MALZEME sütunundaki veriler içerisindeki sayıları, "DEGERLER" isimli sütunda liste halinde ekle
+df["DEGERLER"] = df["MALZEME"].apply(sayi)
+print(df)
+
+# Dosyayı "DEGER.ODS" ismi ile kaydet
+df.to_excel("deger.ods")
